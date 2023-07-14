5b72e9735dc263f288eaf42323bdbaaf3324d3cc
jiinse0 <jee_ji@naver.com>
2023-07-14 18:12:40 +0900

    create, modified

---
+++ src/main/java/com/sparta/spring_lv4/entity/Timestamped.java
@@ -0,0 +1,25 @@
+package com.sparta.spring_lv4.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class Timestamped {
+
+    @CreatedDate
+    @Column(name = "create_date", updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime createAt;
+
+    @LastModifiedDate
+    @Column(name = "modified_date")
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime modifiedAt;
+}
