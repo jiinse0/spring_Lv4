9c306812bdeb804c744669dd7f1c007446d8867d
jiinse0 <jee_ji@naver.com>
2023-07-14 18:15:08 +0900

    댓글 기능 구현

--- src/main/java/com/sparta/spring_lv4/entity/Comment.java
+++ src/main/java/com/sparta/spring_lv4/entity/Comment.java
@@ -1,17 +1,9 @@
 package com.sparta.spring_lv4.entity;
 
 import com.sparta.spring_lv4.dto.CommentRequestDto;
-import com.sparta.spring_lv4.dto.PostRequestDto;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @NoArgsConstructor
