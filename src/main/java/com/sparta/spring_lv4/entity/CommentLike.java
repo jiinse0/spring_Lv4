a07df408ccfb83de41c1e8511d0e0b2c44c9d693
jiinse0 <jee_ji@naver.com>
2023-07-25 16:44:03 +0900

    [feat] 댓글 좋아요 기능 구현

---
+++ src/main/java/com/sparta/spring_lv4/entity/CommentLike.java
@@ -0,0 +1,33 @@
+package com.sparta.spring_lv4.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Table(name = "Comment_like_info")
+public class CommentLike extends Timestamped{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_like")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "comment_id")
+    private Comment comment;
+
+    public CommentLike(User user, Comment comment) {
+        this.user = user;
+        this.comment = comment;
+    }
+
+}
