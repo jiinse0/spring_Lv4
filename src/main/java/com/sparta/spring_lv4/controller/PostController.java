2208442f5969fb1915e58745b48a33cf5a7bc5d0
jiinse0 <jee_ji@naver.com>
2023-07-25 16:46:41 +0900

    [feat] 게시글 좋아요 기능 구현

--- src/main/java/com/sparta/spring_lv4/controller/PostController.java
+++ src/main/java/com/sparta/spring_lv4/controller/PostController.java
@@ -2,9 +2,11 @@ package com.sparta.spring_lv4.controller;
 
 import com.sparta.spring_lv4.dto.PostRequestDto;
 import com.sparta.spring_lv4.dto.PostResponseDto;
+import com.sparta.spring_lv4.dto.StatusResponseDto;
 import com.sparta.spring_lv4.security.UserDetailsImpl;
 import com.sparta.spring_lv4.service.PostService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -48,13 +50,39 @@ public class PostController {
     }
 
     @DeleteMapping("/post/{id}")
-    public ResponseEntity<PostResponseDto> deletePost(@AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable Long id) {
+    public ResponseEntity<StatusResponseDto> deletePost(@AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable Long id) {
 
         try {
-            PostResponseDto deletePost = postService.deletePost(userDetails.getUser(), id);
-            return ResponseEntity.ok().body(deletePost);
+            postService.deletePost(userDetails.getUser(), id);
+            return ResponseEntity.ok().body(new StatusResponseDto("삭제 되었습니다.", HttpStatus.OK.value()));
         } catch (IllegalArgumentException e) {
-            return ResponseEntity.badRequest().build();
+            return ResponseEntity.badRequest().body(new StatusResponseDto("게시글이 존재하지 않거나 삭제할 권한이 없습니다.", HttpStatus.BAD_REQUEST.value()));
         }
+    }
+
+    // 게시글 좋아요
+    @PostMapping("/post/{postId}/like")
+    public ResponseEntity<StatusResponseDto> postLike(@PathVariable Long postId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+
+        try {
+            postService.postLike(postId, userDetails.getUser());
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(new StatusResponseDto(e.getMessage(), HttpStatus.BAD_REQUEST.value()));
+        }
+
+        return ResponseEntity.ok().body(new StatusResponseDto("좋아요 되었습니다.", HttpStatus.OK.value()));
+    }
+
+    // 게시글 좋아요 취소
+    @DeleteMapping("/post/{postId}/like")
+    public ResponseEntity<StatusResponseDto> cancelPostLike(@PathVariable Long postId,  @AuthenticationPrincipal UserDetailsImpl userDetails) {
+
+        try {
+            postService.cancelPostLike(postId, userDetails.getUser());
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(new StatusResponseDto(e.getMessage(), HttpStatus.BAD_REQUEST.value()));
+        }
+
+        return ResponseEntity.ok().body(new StatusResponseDto("좋아요 취소 되었습니다.", HttpStatus.OK.value()));
     }
 }
--- src/main/java/com/sparta/spring_lv4/service/PostService.java
+++ src/main/java/com/sparta/spring_lv4/service/PostService.java
@@ -3,20 +3,24 @@ package com.sparta.spring_lv4.service;
 import com.sparta.spring_lv4.dto.PostRequestDto;
 import com.sparta.spring_lv4.dto.PostResponseDto;
 import com.sparta.spring_lv4.entity.Post;
+import com.sparta.spring_lv4.entity.PostLike;
 import com.sparta.spring_lv4.entity.User;
 import com.sparta.spring_lv4.entity.UserRoleEnum;
+import com.sparta.spring_lv4.repository.PostLikeRepository;
 import com.sparta.spring_lv4.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class PostService {
 
     private final PostRepository postRepository;
+    private final PostLikeRepository postLikeRepository;
 
     public PostResponseDto createPost(PostRequestDto requestDto, User user) {
         Post post = new Post(requestDto, user);
@@ -29,7 +33,7 @@ public class PostService {
     public List<PostResponseDto> getPost() {
         return postRepository.findAllByOrderByCreateAtDesc()
                 .stream()
-                .map(PostResponseDto :: new)
+                .map(PostResponseDto::new)
                 .toList();
     }
 
@@ -52,14 +56,48 @@ public class PostService {
         return new PostResponseDto(post);
     }
 
-    public PostResponseDto deletePost(User user, Long id) {
+    public void deletePost(User user, Long id) {
         Post post = findByPost(id);
 
         if (!(post.getUsername().equals(user.getUsername()) || user.getRole().equals(UserRoleEnum.ADMIN))) {
             throw new IllegalArgumentException("삭제할 권한이 없습니다.");
         }
 
-        return new PostResponseDto(post);
+        postRepository.delete(post);
+    }
+
+    // 게시글 좋아요
+    @Transactional
+    public void postLike(Long postId, User user) {
+        Post post = findByPost(postId);
+
+        if (user.getUsername().equals(post.getUsername())) {
+
+            throw new IllegalArgumentException("자신의 게시글에는 좋아요를 누를 수 없습니다.");
+
+        } else {
+
+            if (postLikeRepository.findByUserAndPost(user, post).isPresent()) {
+                throw new IllegalArgumentException("이미 좋아요한 게시글 입니다.");
+            } else {
+                PostLike postLike = new PostLike(user, post);
+                postLikeRepository.save(postLike);
+            }
+        }
+    }
+
+    @Transactional
+    public void cancelPostLike(Long postId, User user) {
+        Post post = findByPost(postId);
+
+        Optional<PostLike> postLike = postLikeRepository.findByUserAndPost(user, post);
+
+        if (postLike.isEmpty()) {
+            throw new IllegalArgumentException("좋아요 취소 되어있는 게시물 입니다.");
+        } else {
+            postLikeRepository.delete(postLike.get());
+
+        }
     }
 
     private Post findByPost(Long id) {
@@ -67,4 +105,5 @@ public class PostService {
                 () -> new IllegalArgumentException("등록된 게시글이 없습니다.")
         );
     }
+
 }
